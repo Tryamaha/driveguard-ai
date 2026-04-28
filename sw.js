a17a9a0067a22788aff3fc503875a85bf93320b1
Hasan Yamaner <91383073+Tryamaha@users.noreply.github.com>
2026-04-28 17:07:29 +0300

    Add service worker for offline caching

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "driveguard-v12-offline";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./radars.json",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      }).catch(() => cached);
+    })
+  );
+});
